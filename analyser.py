6d96e18cbee0b9c656ce3cad3a7c30aacc82f2db
Boucher Rubens <boucher.rubens.974@gmail.com>
2021-03-01 23:07:24 +0100

    first commit

--- analyser.py
+++ analyser.py
@@ -6,18 +6,20 @@ from pandas import DataFrame, Series  # for convenience
 import pims
 import trackpy as tp
 import moviepy
+import numba
 
-link = "Sample/video2.mp4"
-diamater = 35
-mass = 4000
+
+link = "Sample/4060.avi"
+diamater =21
+mass = 1000
 
 frames = pims.as_grey(pims.Video(link))
 #On analysela vidéo
 max_deplacement = 5
-f = tp.batch(frames[:300], diamater, minmass=mass)
+f = tp.batch(frames[:], diamater, minmass=mass, engine='numba')
 t = tp.link_df(f, max_deplacement, memory=3)
 t.head()
-t1 = tp.filter_stubs(t, 25)
+t1 = tp.filter_stubs(t, 15)
 # Compare the number of particles in the unfiltered and filtered data.
 print('Before:', t['particle'].nunique())
 print('After:', t1['particle'].nunique())
@@ -31,7 +33,7 @@ plt.figure()
 ax = tp.plot_traj(tm)
 
 
-im = tp.imsd(tm, 1/2, 24)  # microns per pixel = .001, frames per second = 24
+im = tp.imsd(tm, 0.131, 30.28)  # microns per pixel = .001, frames per second = 24
 
 fig, ax = plt.subplots()
 ax.plot(im.index, im, 'k-', alpha=0.1)  # black lines, semitransparent
@@ -44,7 +46,7 @@ ax.set_yscale('log')
 
 
 
-em = tp.emsd(tm, 1/2, 24) # microns per pixel = 100/285., frames per second = 24
+em = tp.emsd(tm, 0.131, 30.28) # microns per pixel = 100/285., frames per second = 24
 
 fig, ax = plt.subplots()
 ax.plot(em.index, em, 'o')
@@ -52,10 +54,10 @@ ax.plot(em.index, em, 'o')
 #ax.set_yscale('log')
 ax.set(ylabel=r'$\langle \Delta r^2 \rangle$ [$\mu$m$^2$]',
        xlabel='lagddddd time $t$')
-ax.set(ylim=(1e-2, 10));
+ax.set(ylim=(1e-2, 10))
 
 
-em = tp.emsd(tm, 1/2, 24)
+em = tp.emsd(tm, 0.131, 30.28)
 plt.figure()
 plt.ylabel(r'$\langle \Delta r^2 \rangle$ [$\mu$m$^2$]')
 plt.xlabel('lag timehfhfhf $t$')
--- config.py
+++ config.py
@@ -2,16 +2,16 @@ import matplotlib.pyplot as plt
 import pims
 import trackpy as tp
 
-link = "Sample/video.mp4"
-diamater = 35
-mass = 4000
+link = "Sample/4060.avi"
+diamater =21
+mass = 1000
 
 frames = pims.as_grey(pims.Video(link))
 # On affiche la première image
 plt.imshow(frames[0])
 plt.show()
-# On localise une première fois
-f = tp.locate(frames[0], diamater, minmass=mass, invert=True)
+# On localise une première fois (false = bright)
+f = tp.locate(frames[0], diamater, minmass=mass, invert=False)
 tp.annotate(f, frames[0])
 # On plot les mass
 fig, ax = plt.subplots()
